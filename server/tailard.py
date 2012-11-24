e8f1052f824c9b12f24ed1faf8aa62d106945b42
Magnus Ljadas <magnus.ljadas@gmail.com>
2012-11-24 15:35:26 +0100

    Format code

--- server/tailard.py
+++ server/tailard.py
@@ -4,10 +4,8 @@ import paramiko
 import select
 import ConfigParser
 
-
 config = ConfigParser.ConfigParser()
 config.read('tailard.cfg')
-
 
 host = config.get('monitor', 'host')
 uname = config.get('monitor', 'uname')
@@ -15,18 +13,17 @@ pword = config.get('monitor', 'pword')
 logfile = config.get('monitor', 'logfile')
 serial_path = config.get('monitor', 'serial_path')
 
-
 p = re.compile('.+ERROR.+')
 
 myserial = serial.Serial(serial_path)
 
 client = paramiko.SSHClient()
 client.load_system_host_keys()
-
 client.connect(host, username=uname, password=pword)
 transport = client.get_transport()
 channel = transport.open_session()
 channel.exec_command('tail -f ' + logfile)
+
 while True:
     rl, wl, xl = select.select([channel],[],[],0.0)
     if len(rl) > 0:
